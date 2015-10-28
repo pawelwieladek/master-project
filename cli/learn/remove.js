bb147d4b35d8ea14c3e217a90da21c63b8893e87
Pawel Wieladek <pawel.wieladek@allegro.pl>
2015-10-28 21:59:54 +0100

    New results

---
+++ cli/learn/remove.js
@@ -0,0 +1,20 @@
+import low from 'lowdb'
+import stdio from 'stdio'
+import ids from './dbs/ids'
+import games from './dbs/games'
+import networks from './dbs/networks'
+
+let options= stdio.getopt({
+  id: {
+    key: 'i',
+    description: 'Id',
+    args: 1,
+    mandatory: true
+  }
+});
+
+let id = parseInt(options.id);
+
+ids('results').remove({ id });
+games('results').remove({ id });
+networks('results').remove({ id });
--- cli/temp.js
+++
@@ -1,8 +0,0 @@
-import low from 'lowdb'
-import ids from './learn/dbs/ids'
-import games from './learn/dbs/games'
-import networks from './learn/dbs/networks'
-
-ids('results').remove({ id: 1445354047405 });
-games('results').remove({ id: 1445354047405 });
-networks('results').remove({ id: 1445354047405 });
